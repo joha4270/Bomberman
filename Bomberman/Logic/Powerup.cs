40d86efab41dab5c9480a8f7809f899132d8e4cd
Johannes Elgaard <joha4270@gmail.com>
2014-12-04 10:52:30 +0100

    Most stuff woking, missing menu & bomb collision

---
+++ Bomberman/Logic/Powerup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bomberman
+{
+    class Powerup : Entity
+    {
+        public float SpeedUp = 0;
+        public int BombUp = 0;
+        public int HpUp = 0;
+        public int PowerUp = 0;
+
+        public override bool OnHit()
+        {
+            return true;
+        }
+    }
+}
